861773b2cf0f0b06289c523e19cbe8109fd6bc2f
milan0827 <milanrokaya2014@gmail.com>
2023-05-18 20:17:42 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const logo = document.querySelector(".logo");
+const nav = document.querySelector(".nav");
+const background = document.querySelector(".background");
+const numbers = document.querySelector(".page__stats--details");
+const speed = 200;
+
+const click = logo.addEventListener("click", () => {
+  nav.classList.add("nav__active");
+  background.classList.add("overlay");
+});
+
+background.addEventListener("click", () => {
+  nav.classList.remove("nav__active");
+  background.classList.remove("overlay");
+  nav.style.transition = "all 0.4s";
+});
